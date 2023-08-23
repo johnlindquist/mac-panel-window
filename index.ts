7d38b56539974442523a2d6abc5bcc83347765bf
John Lindquist <johnlindquist@gmail.com>
2023-08-23 16:46:12 -0600

    fix(api): lowercase addon api

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ const addon = bindings("mac-panel-window.node")
 const isMac = process.platform === "darwin"
 export const makeKeyWindow = window => {
   if (!isMac) return
-  return addon.MakeKeyWindow(window.getNativeWindowHandle())
+  return addon.makeKeyWindow(window.getNativeWindowHandle())
 }
 export const makePanel = window => {
   if (!isMac) return
-  return addon.MakePanel(window.getNativeWindowHandle())
+  return addon.makePanel(window.getNativeWindowHandle())
 }
 export const makeWindow = window => {
   if (!isMac) return
-  return addon.MakeWindow(window.getNativeWindowHandle())
+  return addon.makeKeyWindow(window.getNativeWindowHandle())
 }
